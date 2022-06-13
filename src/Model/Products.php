94c91d986ce86e8095ef80c2ce956962cb7a8df0
forkrz <forkrz8@gmail.com>
2022-06-13 19:44:01 +0200

    model created

--- src/Model/Products.php
+++ src/Model/Products.php
@@ -1,1 +1,17 @@
 <?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    public $casts = [
+        'Name' => 'string',
+        'NET_PRICE' => 'float',
+    ];
+
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    protected $guarded = [];
+}
