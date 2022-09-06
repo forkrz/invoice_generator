870ad4e6d96d96c0e182b0200c244e56a83cdb24
forkrz <forkrz8@gmail.com>
2022-09-06 22:36:31 +0200

    deleting product added

--- assets/js/Products.js
+++ assets/js/Products.js
@@ -3,7 +3,7 @@ import IMask from "imask";
 export class Products {
 
     init() {
-        const productContainer = document.querySelectorAll('.product-select')[document.querySelectorAll('.product-select').length -1];
+        const productContainer = document.querySelectorAll('.product-select')[document.querySelectorAll('.product-select').length - 1];
         let inputLists = productContainer.parentNode.parentNode.querySelectorAll('div');
         let netPriceMask = IMask(
             inputLists[1].querySelector('input'),
@@ -17,12 +17,13 @@ export class Products {
             });
 
         this.addNewProductInput();
-
         this.fillProductData(productContainer, netPriceMask, taxRateMask);
         this.calculateNetValue(document.getElementById('client_user_date_invoice_form_Product___name___Quantity'), document.getElementById('client_user_date_invoice_form_Product___name___NET_PRICE'), document.getElementById('client_user_date_invoice_form_Product___name___NET_VALUE'));
         this.calculateTaxValue(document.getElementById('client_user_date_invoice_form_Product___name___Quantity'), document.getElementById('client_user_date_invoice_form_Product___name___NET_PRICE'), document.getElementById('client_user_date_invoice_form_Product___name___TAX_RATE'), document.getElementById('client_user_date_invoice_form_Product___name___TAX_VALUE'));
-        this.calculateGrossValue(document.getElementById('client_user_date_invoice_form_Product___name___Quantity'), document.getElementById('client_user_date_invoice_form_Product___name___NET_PRICE'), document.getElementById('client_user_date_invoice_form_Product___name___TAX_RATE'),document.getElementById('client_user_date_invoice_form_Product___name___NET_VALUE'), document.getElementById('client_user_date_invoice_form_Product___name___TAX_VALUE'), document.getElementById('client_user_date_invoice_form_Product___name___GROSS_VALUE'));
+        this.calculateGrossValue(document.getElementById('client_user_date_invoice_form_Product___name___Quantity'), document.getElementById('client_user_date_invoice_form_Product___name___NET_PRICE'), document.getElementById('client_user_date_invoice_form_Product___name___TAX_RATE'), document.getElementById('client_user_date_invoice_form_Product___name___NET_VALUE'), document.getElementById('client_user_date_invoice_form_Product___name___TAX_VALUE'), document.getElementById('client_user_date_invoice_form_Product___name___GROSS_VALUE'));
+        this.deleteProduct(document.getElementsByClassName("fa-solid fa-xmark delete-product-icon")[0]);
     }
+
     addNewProductInput() {
         const productsList = document.getElementById('products-list');
         const inputDiv = productsList.getElementsByTagName('div')[3];
@@ -58,8 +59,10 @@ export class Products {
                 });
             this.fillProductData(productContainer, netPriceMask, taxRateMask);
             this.calculateNetValue(inputLists[1].querySelector('input'), inputLists[2].querySelector('input'), inputLists[4].querySelector('input'));
-            this.calculateTaxValue(inputLists[1].querySelector('input'),  inputLists[2].querySelector('input'), inputLists[3].querySelector('input'), inputLists[5].querySelector('input'));
-            this.calculateGrossValue(inputLists[1].querySelector('input'),  inputLists[2].querySelector('input'), inputLists[3].querySelector('input'), inputLists[4].querySelector('input'), inputLists[5].querySelector('input'), inputLists[6].querySelector('input'))
+            this.calculateTaxValue(inputLists[1].querySelector('input'), inputLists[2].querySelector('input'), inputLists[3].querySelector('input'), inputLists[5].querySelector('input'));
+            this.calculateGrossValue(inputLists[1].querySelector('input'), inputLists[2].querySelector('input'), inputLists[3].querySelector('input'), inputLists[4].querySelector('input'), inputLists[5].querySelector('input'), inputLists[6].querySelector('input'));
+            this.deleteProduct(inputLists[8].querySelector('i'));
+            this.toggleDeleteButtons();
         })
     }
 
@@ -91,7 +94,7 @@ export class Products {
                 quantityVal = parseInt(quantityVal);
                 netPriceVal = parseFloat(netPriceVal);
                 if (quantityVal !== 0 && netPriceVal !== 0) {
-                    netValue.value =  (quantityVal * netPriceVal).toFixed(2);
+                    netValue.value = (quantityVal * netPriceVal).toFixed(2);
                 } else {
                     netValue.value = '';
                 }
@@ -155,5 +158,25 @@ export class Products {
         taxRate.addEventListener('change', () => {
             calculate()
         })
+    }
+
+    deleteProduct(el) {
+        el.addEventListener('click', () => {
+            el.parentNode.parentNode.remove();
+            this.toggleDeleteButtons();
+        })
+    }
+
+    toggleDeleteButtons() {
+        const deleteButtons= document.getElementsByClassName("fa-solid fa-xmark delete-product-icon");
+        if(deleteButtons.length > 1) {
+            Array.from(deleteButtons).forEach((el) =>{
+                el.style.display = 'block'
+            })
+        } else {
+            Array.from(deleteButtons).forEach((el) =>{
+                el.style.display = 'none'
+            })
+        }
     }
 }
