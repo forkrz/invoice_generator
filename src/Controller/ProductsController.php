ab27ea145ec1af210e4c8d35fda9127db02a73fa
forkrz <forkrz8@gmail.com>
2022-09-14 20:19:24 +0200

    add products styles

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -25,7 +25,7 @@ Class ProductsController extends AbstractController
             $formData = $request->request->all()['products_form'];
             $products = new Products();
             $products->NAME = $formData['NAME'];
-            $products->PRICE =(float) $formData['PRICE'];
+            $products->NET_PRICE =(float) $formData['NET_PRICE'];
             $products->TAX_RATE = $formData['TAX_RATE'];
             $products->User_ID = $this->getuser()->getId();
             $products->save();
@@ -84,7 +84,7 @@ Class ProductsController extends AbstractController
         $editedProductsData = Products::query()
             ->where('ID', $id)
             ->where('USER_ID',$this->getUser()->getId())
-            ->select('NAME','PRICE')
+            ->select('NAME','NET_PRICE', 'TAX_RATE')
             ->first();
 
         if(empty($editedProductsData))
