c83d60e94139f9d075728ba20dc8bf8cbb48e42b
forkrz <forkrz8@gmail.com>
2022-09-22 21:05:23 +0200

    heroku

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,6 +10,16 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    #[Route(path:'', name: 'index')]
+    public function index():Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('invoice/show');
+        } else{
+            return $this->redirectToRoute('app_login');
+        }
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
