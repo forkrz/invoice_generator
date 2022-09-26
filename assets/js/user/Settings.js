5a96dca163162a34259a619d6cd13ee88265d0c4
forkrz <forkrz8@gmail.com>
2022-09-26 22:16:52 +0200

    add error msgs for clients pages

--- assets/js/user/Settings.js
+++ assets/js/user/Settings.js
@@ -12,63 +12,69 @@ export class Settings {
         return !el.value.trim().length;
     }
 
-    toggleErrorClass(el, isOk) {
-        !isOk ? el.classList.add('error') : el.classList.remove('error');
+    toggleErrorInfo(element,message, isOk) {
+        !isOk ? element.classList.add('error') : element.classList.remove('error');
+        !isOk ? message.style.display = 'block' : message.style.display = 'none';
     }
+
 
     checkNip() {
         const nip = document.getElementById('NIP');
+        const errorMsg = document.getElementById('nip-error');
         if (!this.checkIfEmpty(nip)) {
             const tenNumbersRegex = /^[0-9]{10}$/;
             const regex = new RegExp(tenNumbersRegex);
             const NipCheck = regex.test(nip.value);
-            this.toggleErrorClass(nip, NipCheck);
+            this.toggleErrorInfo(nip,errorMsg, NipCheck);
             return NipCheck;
         } else {
-            this.toggleErrorClass(nip, false);
-            return false;
+            this.toggleErrorInfo(nip,errorMsg, false);
         }
     }
 
     checkZipCode() {
         const zipCode = document.getElementById('ZIP');
+        const errorMsg = document.getElementById('zip-code-error');
         if (!this.checkIfEmpty(zipCode)) {
             const zipCodeRegex = /\d{2}-\d{3}/;
             const regex = new RegExp(zipCodeRegex);
             const zipCodeCheck = regex.test(zipCode.value);
-            this.toggleErrorClass(zipCode, zipCodeCheck);
+            this.toggleErrorInfo(zipCode,errorMsg, zipCodeCheck);
             return zipCodeCheck;
         } else {
-            this.toggleErrorClass(zipCode, false);
-            return false;
+            this.toggleErrorInfo(zipCode,errorMsg, false);
         }
     }
 
     checkName() {
         const name = document.getElementById('NAME');
+        const errorMsg = document.getElementById('name-error');
         const check = !this.checkIfEmpty(name)
-        this.toggleErrorClass(name, check);
+        this.toggleErrorInfo(name,errorMsg, check);
         return check;
     }
 
     checkStreet() {
         const name = document.getElementById('STREET');
+        const errorMsg = document.getElementById('street-error');
         const check = !this.checkIfEmpty(name)
-        this.toggleErrorClass(name, check);
+        this.toggleErrorInfo(name,errorMsg, check);
         return check;
     }
 
     checkCity() {
         const name = document.getElementById('CITY');
+        const errorMsg = document.getElementById('city-error');
         const check = !this.checkIfEmpty(name)
-        this.toggleErrorClass(name, check);
+        this.toggleErrorInfo(name,errorMsg, check);
         return check;
     }
 
     checkEmail() {
         const name = document.getElementById('EMAIL');
+        const errorMsg = document.getElementById('email-error');
         const check = !this.checkIfEmpty(name)
-        this.toggleErrorClass(name, check);
+        this.toggleErrorInfo(name,errorMsg, check);
         return check;
     }
 }
